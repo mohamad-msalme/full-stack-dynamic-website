8f808e71d0f223b089c2c314c4655eaf30ff3f41
Mohamad Masalmeh <mohamad.msalme77@gmail.com>
2024-03-15 17:38:06 +0300

    add build

--- server/dist/middlewares/auth.middleware.js
+++ server/dist/middlewares/auth.middleware.js
@@ -6,11 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.authenticateUser = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const authenticateUser = (req, res, next) => {
+    var _a;
     try {
-        console.log(req.cookies);
-        const token = req.cookies.token;
+        const token = (_a = req === null || req === void 0 ? void 0 : req.cookies) === null || _a === void 0 ? void 0 : _a.token;
         if (!token) {
-            return res.status(401).send({ error: {
+            return res.status(401).json({ error: {
                     message: "Unauthorized"
                 } });
         }
