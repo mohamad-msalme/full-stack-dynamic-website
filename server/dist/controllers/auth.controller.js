1d44c7774c28598178e5d508355a777a2650d4e9
Mohamad Masalmeh <mohamad.msalme77@gmail.com>
2024-03-15 21:02:24 +0300

    push the build file

--- server/dist/controllers/auth.controller.js
+++ server/dist/controllers/auth.controller.js
@@ -76,7 +76,7 @@ const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: "lax"
+            sameSite: "strict"
         });
         return res.status(200).send({
             success: {
